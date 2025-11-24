ecdf8602e3969495b044f7d21d06933517a96acc
rahmad-ctrl <davieddavied57@gmail.com>
2025-11-24 16:26:56 +0700

    update full navbar

--- src/script/index.js
+++ src/script/index.js
@@ -1,20 +1,45 @@
 export function nav(){
+  let menu = document.querySelector('.nav-menu');
+  let menuIcon = document.querySelector('i');
+  let navBtn = document.getElementById('navBtn');
   window.navBtn = function (){
-    let menu = document.querySelector('.nav-menu');
-    let menuIcon = document.querySelector('i');
-    let navBtn = document.getElementById('navBtn')
     menu.classList.toggle('menuOn');
     if (menu.classList.contains('menuOn')) {
-      menuIcon.classList.remove('fa-bars');
-      menuIcon.classList.add('fa-xmark');
-      menuIcon.style.transform ='rotate(360deg) scale(1.1)'
-    } else {
-      menuIcon.classList.add('fa-bars');
-      menuIcon.classList.remove('fa-xmark');
-      menuIcon.style.transform ='rotate(0deg) scale(1)';
+    menuIcon.classList.replace('fa-bars','fa-xmark');
+    menuIcon.style.transform = 'rotate(180deg) scale(1.1)'
+  } else {
+    menuIcon.classList.replace('fa-xmark','fa-bars');
+    menuIcon.style.transform = 'rotate(0deg) scale(1)';
     };
-    setTimeout(() => {
-      menuIcon.style.transform =' '
-    },500);
-  }
+  };
+    window.ke = function (target){
+      if (typeof target === 'string') {
+        let element = document.querySelector(target);
+        if (target) {
+          element.scrollIntoView({
+            behavior: 'smooth',
+            block : 'start'
+          })
+        }
+      }
+      tutup()
+    };
+    window.tutup = function (){
+      menu.classList.remove('menuOn')
+      menuIcon.classList.replace('fa-xmark','fa-bars');
+      menuIcon.style.transform = 'rotate(0deg)'
+    }
+    window.darkOn = function (){
+      let body = document.querySelector('body');
+      let iconDrk = document.getElementById('iconDrk');
+      body.classList.toggle('darkOn');
+      if (body.classList.contains('darkOn')) {
+        iconDrk.classList.replace('fa-moon','fa-sun');
+        iconDrk.style.transform = 'rotate(360deg)'
+      } else {
+        iconDrk.classList.replace('fa-sun','fa-moon');
+        iconDrk.style.transform = 'rotate(0deg)'
+      }
+      
+    }
 };
